59b0560664f0bea0271bd162a3160095c88da912
sumin-yang224 <kzx1547p@gmail.com>
2022-05-05 08:52:32 -0700

    [fix] macro 수정

--- src/include/macro.h
+++ src/include/macro.h
@@ -2,9 +2,11 @@
 #define FALSE 0
 
 #define ID_SIZE 16
-#define CHALLENGE_TS_SIZE 16
+#define CHALLENGE_SIZE 16
+#define TIMESTAMP_SIZE 16
 #define NAME_SIZE 20
 #define SYM_KEY_SIZE 32
+#define PRINT_LINE_SIZE 32
 #define HASH_SIZE 32
 #define AUTH_MSG_SIZE 64
 #define TOKEN_SIZE 64
@@ -13,8 +15,11 @@
 #define SYM_KEY_BIT 256
 #define RSA_PUB_KEY_SIZE 512
 
+
 #define BUF_SIZE 1024
+#define RSA_LENGTH 2048
 #define PUBLIC_KEY_BITS 2056
 
 #define FILE_SIZE 1000000
 
+#define NEED_TOKEN_MSG "I need a token."
